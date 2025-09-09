68ff7204ac45573a324f351e728261154776cfd0
Asko Soukka <asko.soukka@iki.fi>
2025-09-09 21:23:44 +0300

    Add uid and parent_uid substitutions content references

---
+++ src/collective/webhook/adapters.py
@@ -0,0 +1,25 @@
+from Acquisition import aq_parent
+from collective.webhook import _
+from plone.stringinterp.adapters import BaseSubstitution
+from plone.stringinterp.interfaces import IStringSubstitution
+from plone.uuid.interfaces import IUUID
+from zope.component import adapter
+from zope.interface import Interface
+
+
+@adapter(Interface)
+class UUIDSubstitution(BaseSubstitution):
+    category = _("All Content")
+    description = _("Identifier of the content or login of managed user")
+
+    def safe_call(self):
+        return IUUID(self.context)
+
+
+@adapter(Interface)
+class ParentUUIDSubstitution(BaseSubstitution):
+    category = _("All Content")
+    description = _("Identifier of the parent content or login of managed user")
+
+    def safe_call(self):
+        return IUUID(aq_parent(self.context))
